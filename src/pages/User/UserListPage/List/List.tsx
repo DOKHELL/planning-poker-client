8c9b5e6dc85ec449c611f0e480c4c7d7ee973bc0
Yevhenii Nechytailo <yevhenii.nechytailo@cd.p2h.com>
2023-01-19 15:53:35 +0200

    fix: remove commented code, change useNavigate hook to custom useNavigation

--- src/pages/User/UserListPage/List/List.tsx
+++ src/pages/User/UserListPage/List/List.tsx
@@ -3,15 +3,11 @@ import styles from './List.module.scss'
 import { useListUser } from '@/hooks/queries/user.query'
 import BlockLoader from '@/components/BlockLoader/BlockLoader'
 import { USER_DETAILS } from '@/constants/routes'
-import { useNavigate } from 'react-router-dom'
+import useNavigation from '@/hooks/useNavigation'
 
 const List = () => {
-  const navigate = useNavigate()
+  const { handlePushAutoCall } = useNavigation()
   const { data, isFetching, error } = useListUser()
-
-  const handleRedirect = (id: string) => () => {
-    navigate(USER_DETAILS(id))
-  }
 
   if (error) {
     return <div>Error: {error.message}</div>
@@ -34,7 +30,7 @@ const List = () => {
           <>
             {isFetching && <tr><td className={styles.loader}><BlockLoader/></td></tr>}
             {data && !!data?.users.length ? data?.users.map(obj => (
-              <tr key={obj.id} onClick={handleRedirect(obj.id)}>
+              <tr key={obj.id} onClick={handlePushAutoCall(USER_DETAILS(obj.id))}>
                 <td>{obj.id}</td>
                 <td>{obj.name}</td>
                 <td>{obj.email}</td>
@@ -47,35 +43,6 @@ const List = () => {
         </tbody>
       </table>
     </div>
-    // <table className={styles.table}>
-    //   <thead className={styles.thead}>
-    //     <tr className={styles.trHead}>
-    //       {keys.length ? keys.map((item, index) => (
-    //         <th className={styles.th} key={index}>
-    //           {item}
-    //         </th>
-    //       )): <th className={styles.th}>No Found</th>}
-    //     </tr>
-    //   </thead>
-    //   <tbody className={styles.tbody}>
-    //     <>
-    //       {isFetching && <tr><td><BlockLoader/></td></tr>}
-    //       {data && !!data?.users.length ? data?.users.map((obj, index) => (
-    //         <tr className={styles.trBody} key={index}>
-    //           {!!keys?.length && keys.map((item: string, index: number) => {
-    //             // @ts-ignore
-    //             const value = obj[item]
-    //             return (
-    //               <td className={styles.td} key={index} onClick={handleRedirect(obj.id)}>
-    //                 {value}
-    //               </td>
-    //             )
-    //           })}
-    //         </tr>
-    //       )) : <tr><td className={styles.td}>No Found</td></tr>}
-    //     </>
-    //   </tbody>
-    // </table>
   )
 }
 

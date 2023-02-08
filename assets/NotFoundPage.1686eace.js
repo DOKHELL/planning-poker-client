cf468ba57a17bb2366ae9e11d5eac625a20f486a
Serhii Hladkov <serhii.hladkov@p2h.com>
2023-02-08 22:04:22 +0200

    Updates

---
+++ assets/NotFoundPage.1686eace.js
@@ -0,0 +1,1 @@
+import{u as o,j as t,F as a}from"./index.ee897845.js";const r=()=>{const{t:n}=o();return t(a,{children:t("h1",{children:n("notFound.title")})})};export{r as default};
--- assets/NotFoundPage.86083260.js
+++
@@ -1,1 +0,0 @@
-import{u as o,j as t,F as a}from"./index.cf514004.js";const r=()=>{const{t:n}=o();return t(a,{children:t("h1",{children:n("notFound.title")})})};export{r as default};

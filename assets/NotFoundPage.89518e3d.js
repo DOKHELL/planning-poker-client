4c9da6cc81645f8826ec323030e9009c5461b707
Serhii Hladkov <serhii.hladkov@p2h.com>
2023-02-09 01:11:43 +0200

    Updates

---
+++ assets/NotFoundPage.89518e3d.js
@@ -0,0 +1,1 @@
+import{b as o,j as t,F as a}from"./index.57579a59.js";const s=()=>{const{t:n}=o();return t(a,{children:t("h1",{children:n("404 Not Found")})})};export{s as default};
--- assets/NotFoundPage.939281bc.js
+++
@@ -1,1 +0,0 @@
-import{b as o,j as t,F as a}from"./index.4e63dfed.js";const s=()=>{const{t:n}=o();return t(a,{children:t("h1",{children:n("404 Not Found")})})};export{s as default};
